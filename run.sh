cce4c5a4f616a4e0a45b14ae2f330f3ddbce636b
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-28 20:39:24 +0200

    Use venv to install

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xe
-# pip-3 install -r requirements.txt
-FLASK_APP=example.py python3 -m flask.cli run --host 0.0.0.0 --port 80
+virtualenv-3 venv
+venv/bin/pip3 install -r requirements.txt
+FLASK_APP=example.py venv/bin/python3 -m flask.cli run --host 0.0.0.0 --port 80
